4373064f0a9c6fc8ed6149fb025392378ce8b8a2
Yash <yashpatel54257@gmail.com>
2024-06-13 08:30:23 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,21 +11,14 @@ const Navbar = () => {
     setIsMenuClicked(!isMenuClicked);
   };
 
-  const handleNavLinkClick = (e) => {
-    const links = document.querySelectorAll('.navlink');
-    links.forEach(link => link.classList.remove('clicked'));
-    e.target.classList.add('clicked');
-  };
-
-
   return (
     <div className='both-nav'>
       <div className='navbar'>
-        <a href='#about' className='navlink' onClick={handleNavLinkClick}>About</a>
-        <a href='#skill' className='navlink' onClick={handleNavLinkClick}>Skill</a>
-        <a href='#education' className='navlink' onClick={handleNavLinkClick}>Education and Experience</a>
-        <a href='#projects' className='navlink' onClick={handleNavLinkClick}>Projects</a>
-        <a href='#contact' className='navlink' onClick={handleNavLinkClick}>Contact</a>
+        <a href='#about' className='navlink'>About</a>
+        <a href='#skill' className='navlink'>Skill</a>
+        <a href='#education' className='navlink'>Education and Experience</a>
+        <a href='#projects' className='navlink'>Projects</a>
+        <a href='#contact' className='navlink'>Contact</a>
       </div>
       <div className='res-container'>
         <h3><img className='logo' src={yp} alt='yp' /></h3>
@@ -35,11 +28,11 @@ const Navbar = () => {
       </div>
 
       <div className={`res-nav ${isMenuClicked ? 'active' : ''}`}>
-        <a href='#about' className='navlink' onClick={handleNavLinkClick}>About</a>
-        <a href='#skill' className='navlink' onClick={handleNavLinkClick}>Skill</a>
-        <a href='#education' className='navlink' onClick={handleNavLinkClick}>Education and Experience</a>
-        <a href='#projects' className='navlink' onClick={handleNavLinkClick}>Projects</a>
-        <a href='#contact' className='navlink' onClick={handleNavLinkClick}>Contact</a>
+        <a href='#about' className='navlink'>About</a>
+        <a href='#skill' className='navlink'>Skill</a>
+        <a href='#education' className='navlink'>Education and Experience</a>
+        <a href='#projects' className='navlink'>Projects</a>
+        <a href='#contact' className='navlink'>Contact</a>
       </div>
     </div>
   );
